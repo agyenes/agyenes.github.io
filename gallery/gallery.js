8b714e28599f80cf9ad28bd018c8cdaa58416683
agyenes <aron.gyenes@gmail.com>
2016-07-01 15:10:10 +0200

    gallery working OK

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,16 +1,16 @@
 'use strict';
 
 var images = [
-    "Images/img1.jpg",
-    "Images/img2.jpg",
-    "Images/img3.jpg",
-    "Images/img4.jpg",
-    "Images/img5.jpg",
-    "Images/img6.jpg",
-    "Images/img7.jpg",
-    "Images/img8.jpg",
-    "Images/img9.jpg",
-    "Images/img10.jpg",
+    "./Images/img1.jpg",
+    "./Images/img2.jpg",
+    "./Images/img3.jpg",
+    "./Images/img4.jpg",
+    "./Images/img5.jpg",
+    "./Images/img6.jpg",
+    "./Images/img7.jpg",
+    "./Images/img8.jpg",
+    "./Images/img9.jpg",
+    "./Images/img10.jpg"
 ]
 
 var picture_titles = [
