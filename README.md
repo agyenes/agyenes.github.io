3171c3d4e58c5f87fc39326ae95daa9368c83363
agyenes <aron.gyenes@gmail.com>
2016-08-22 08:59:19 +0200

    euedge test app without add function

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Currently uploaded stuff in my repo:
 
+euedgetest: http://agyenes.github.io/euedgetest/index.html
+
 gallery: http://agyenes.github.io/gallery/gallery.html
 
 wikipedia: http://agyenes.github.io/wikipedia/gya_wiki.html
@@ -9,3 +11,5 @@ cv: http://agyenes.github.io/gya_cv/gya_cv.html
 yodanizer: http://agyenes.github.io/yodanizer/yoda.html
 
 todo app: http://agyenes.github.io/todo/frontend/index.html
+
+todo app: http://agyenes.github.io/meal_app/client/index.html
---
+++ euedgetest/app.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var frontendTestApp = angular.module('frontendTestApp', ['ui.bootstrap']);
+
+frontendTestApp.controller('testAppController', ['$scope', '$modal', '$log', '$http',
+
+  function($scope, $modal, $log, $http) {
+    $scope.removePerson = function(person) {
+      var removedPerson = $scope.persons.indexOf(person);
+      $scope.persons.splice(removedPerson, 1);
+    };
+
+    function personAdder() {
+      return {
+        name: $scope.newperson.name,
+        job: $scope.newperson.job,
+        age: $scope.newperson.age,
+        nick: $scope.newperson.nick,
+        employee: $scope.newperson.employee
+      };
+    }
+
+    function clearInputFields() {
+      $scope.newperson.name = '';
+      $scope.newperson.job = '';
+      $scope.newperson.age = '';
+      $scope.newperson.nick = '';
+      $scope.newperson.employee = '';
+    }
+
+    $scope.addPerson = function() {
+      var personToAdd = personAdder();
+      $scope.persons.push(personToAdd);
+      clearInputFields();
+    };
+
+    $http.get('data/persons.json').success(function(data) {
+      $scope.persons = data;
+    });
+
+    $scope.showForm = function() {
+      var modalInstance = $modal.open({
+        templateUrl: 'modal-form.html',
+        controller: ModalInstanceCtrl,
+        scope: $scope,
+        resolve: {
+          userForm: function() {
+            return $scope.userForm;
+          }
+        }
+      });
+
+      modalInstance.result.then(function(selectedItem) {
+        $scope.selected = selectedItem;
+      }, function() {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+  }]);
+
+var ModalInstanceCtrl = function($scope, $modalInstance, userForm) {
+
+  $scope.form = {};
+
+  $scope.submitForm = function() {
+    $modalInstance.close('closed');
+  };
+
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancel');
+  };
+};
