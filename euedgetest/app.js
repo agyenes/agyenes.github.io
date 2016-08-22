c4c94b26fa4c21e74d92221c7628a56ee82d4c2e
agyenes <aron.gyenes@gmail.com>
2016-08-22 15:26:36 +0200

    add sortable name column

--- euedgetest/app.js
+++ euedgetest/app.js
@@ -63,6 +63,7 @@ var ModalInstanceCtrl = function($scope, $modalInstance, userForm) {
   $scope.form = {};
 
   $scope.submitForm = function() {
+    // $scope.addPerson();
     $modalInstance.close('closed');
   };
 
